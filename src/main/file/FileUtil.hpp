05dc174314cc3a63c04401159c72815a7373aa12
Izmar Verhage <izmar.verhage@lunatech.nl>
2019-04-06 10:11:31 +0200

    Add path join method

--- src/main/file/FileUtil.hpp
+++ src/main/file/FileUtil.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <iostream>
+#include <sstream>
 
 #include <stdio.h>
 
@@ -14,7 +16,26 @@ namespace moduru {
 			static std::vector<std::string> splitName(std::string name);
 			static int GetLastSeparator(std::string path);
 			static bool IsDirectory(std::string path);
-			static std::string getSeparator();
+
+            // Returns path separator used by the OS
+            static std::string getSeparator();
+            
+            // Joins names, welded using path separator
+            template<typename ... String_Types, typename Separator>
+            static std::string join_with_separator(Separator separator, String_Types ... strings)
+            {
+                std::vector<std::string> string_array = {strings...};
+                std::stringstream new_string;
+                for(auto &s : string_array)
+                {
+                    if (s != string_array.back()) {
+                        new_string<<s<<separator;
+                    }
+                    else {
+                        new_string<<s;
+                    }
+                }
+            }
 
 		};
 
