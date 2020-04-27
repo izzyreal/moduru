860fb3fcf129acb55ebb4660b7defd27fd62e6c1
Izmar Verhage <izmaelverhage@gmail.com>
2020-04-27 09:43:49 +0200

    Foo

--- src/main/sys/Home.cpp
+++ src/main/sys/Home.cpp
@@ -1,9 +1,17 @@
+#define _HAS_STD_BYTE 0
 #define _CRT_SECURE_NO_WARNINGS
 #include <sys/Home.hpp>
+
+#include <Logger.hpp>
 
 #ifdef __APPLE__
 #include <pwd.h>
 #include <unistd.h>
+#endif
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <ShlObj.h>
 #endif
 
 using namespace moduru::sys;
@@ -17,16 +25,25 @@ using namespace std;
 
 #ifdef _WIN32
 string Home::get() {
-	auto homeDrive = string(getenv("HOMEDRIVE"));
-	auto homePath = string(getenv("HOMEPATH"));
-	auto oldResult = homeDrive + homePath;
-	return u8"C:\\Users\\Izm¿r";
+
+    char path[MAX_PATH];
+    if (SHGetFolderPathA(NULL, CSIDL_PROFILE, NULL, 0, path) != S_OK)
+    {
+        return string(path);
+    }
+    else
+    {
+        auto homeDrive = string(getenv("HOMEDRIVE"));
+        auto homePath = string(getenv("HOMEPATH"));
+        auto oldResult = homeDrive + homePath;
+        return homeDrive + homePath;
+    }
 }
 #else
 string Home::get() {
-	char* home = getpwuid(getuid())->pw_dir;
-	string res = string(home);
-	//return res;
-	return u8"/Users/Izm\u00F8r";
+    char* home = getpwuid(getuid())->pw_dir;
+    string res = string(home);
+    return res;
+    //return u8"/Users/Izm\u00F8r";
 }
 #endif
