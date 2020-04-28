7b9d8e8cdae90d1beca9ddcacb8b03358c88f134
Izmar Verhage <izmaelverhage@gmail.com>
2020-04-29 01:17:31 +0300

    Add debug to build script

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,13 @@
 rm -rf ~/.conan/data/moduru
 rm -rf ~/git/moduru/build/Release/*
+rm -rf ~/git/moduru/build/Debug/*
 
 cd ~/git/moduru/build/Release
 
+conan install -s build_type=Debug ../../ moduru/0.1@izmar/dev
+conan build ../../
+
 conan install -s build_type=Release ../../ moduru/0.1@izmar/dev
 conan build ../../
+
 Conan create ../../ moduru/0.1@izmar/dev
