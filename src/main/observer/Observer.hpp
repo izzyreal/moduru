1f9309336b67953b682be9fa4ecdb76de7c0a40e
Izmar Verhage <izmaelverhage@gmail.com>
2021-02-07 12:39:20 +0100

    Format

--- src/main/observer/Observer.hpp
+++ src/main/observer/Observer.hpp
@@ -2,21 +2,18 @@
 #include <string>
 #include <thirdp/any.hpp>
 
-namespace moduru {
+namespace moduru::observer
+{
+class Observable;
+}
 
-	namespace observer {
-
-		class Observable;
-
-		class Observer {
-
-		public:
-            virtual void update(nonstd::any) {}
-			virtual void update(Observable* obs, nonstd::any arg) {}
-
-		public:
-			virtual ~Observer() {}
-		};
-
-	}
+namespace moduru::observer
+{
+class Observer
+{
+public:
+    virtual void update(nonstd::any) {}
+    virtual void update(Observable*, nonstd::any) {}
+    virtual ~Observer() {}
+};
 }
