6a144fd4b5ef4e94da1ad47be0bd030f64f957e1
Izmar Verhage <izmaelverhage@gmail.com>
2023-08-06 16:35:15 +0200

    Decommission demangle

--- src/main/System.cpp
+++ src/main/System.cpp
@@ -3,10 +3,6 @@
 #include <chrono>
 #include <thread>
 
-#if defined (__APPLE__) || defined (__linux__)
-#include <cxxabi.h>
-#endif
-
 using namespace std;
 using namespace std::chrono;
 using namespace moduru;
@@ -15,26 +11,6 @@ uint64_t System::currentTimeMillis()
 {
 	return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 }
-
-#if defined (__APPLE__) || defined (__linux__)
-string System::demangle(const string& typeIdName) {
-    int status = 0;
-    char* demangled = abi::__cxa_demangle(typeIdName.c_str(), 0, 0, &status);
-    std::string result(demangled);
-    free(demangled);
-    return result;
-}
-#endif
-
-#ifdef _WIN32
-string System::demangle(const string& typeIdName) {
-    auto copy = string(typeIdName);
-    auto toDelete = string("class ");
-    auto i = copy.find(toDelete);
-    if (i != string::npos) copy.erase(i, toDelete.size());
-    return copy;
-}
-#endif // _WIN32
 
 void System::sleep(const int ms) {
 	this_thread::sleep_for(chrono::milliseconds(ms));
--- src/main/System.hpp
+++ src/main/System.hpp
@@ -9,7 +9,6 @@ namespace moduru {
 
 	public:
 		static uint64_t currentTimeMillis();
-		static std::string demangle(const std::string& typeIdName);
 		static void sleep(const int ms);
 
 	};
